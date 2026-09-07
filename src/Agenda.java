564c07f84ec0deb0c1f893af93ef2da83b7fbec3
Jose Esquivel <jose.esquivel.a7x@gmail.com>
2026-09-07 12:55:46 -0600

    se añadio la indicacion de cuantos más se pueden ingresar a la lista de contactos

--- src/Agenda.java
+++ src/Agenda.java
@@ -21,4 +21,9 @@ public class Agenda {
         }
         return añadido;
     }
+    
+    // Indica cuántos contactos más se pueden ingresar
+    public int espacioLibres() {
+        return tamanoMaximo - contactos.size();
+    }
 }
